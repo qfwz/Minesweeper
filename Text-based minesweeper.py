dc54d7ec454d06e61c26fc02a1039c9f8e1643bd
Fawwaz <qmazfawwaz@gmail.com>
2026-05-11 14:48:10 +0700

    Add multiple box size and more features
    - feat: now player can choose box size
    - feat: remove flags feature

--- Text-based minesweeper.py
+++ Text-based minesweeper.py
@@ -1,19 +1,20 @@
 import os
 import random
 
-#5x5 grid
-line = "- - - - - - - - - - - - - - -"
+from matplotlib.pylab import diff, size
+
 box = ["💣", "⬛" , "💥" , "🚩", "⬜","🟨"]
 numbers = ["0️⃣ ","1️⃣ ","2️⃣ ","3️⃣ ","4️⃣ ","5️⃣ ","6️⃣ ","7️⃣ ","8️⃣ "]
 
 
-isi = [[],[],[],[],[]]
+inside = [[],[],[],[],[]]
 
-table = [   [],
-            [],
-            [],
-            [],
-            []    ]
+play_box = []
+
+size = int(input("Choose box size:\n1.4x4\n2.6x6\n3.8x8\npick:")) + 2
+
+for i in range(size):
+    play_box.append([])
 
 dirs = [
     (-1,-1), (-1,0), (-1,1),
@@ -22,28 +23,36 @@ dirs = [
 ]
 
 #table
-for i in range(5):
-    for j in range(5):
+for i in range(size):
+    for j in range(size):
         chance = random.randint(1,100)
         if chance < 30:
-            isi[i].append(box[0])
+            inside[i].append(box[0])
         else:
-            isi[i].append(box[1])
+            inside[i].append(box[1])
             
-for i in range(5):
-    for j in range(5):
-        table[i].append(box[4])
+for i in range(size):
+    for j in range(size):
+        play_box[i].append(box[4])
         
         
 #board
 def board():
-    print(f"    K1   K2   K3   K4   K5", end="\n")
-    for i in range(5):
-        for j in range(5):
-            if j == 0:
-                print(f"B{i+1}",end="")
-            print(f"|{table[i][j]:^4}" ,end="")
-        print(f"|\n{line:^20}")
+    print("   ", end="")
+    
+    # header kolom
+    for i in range(size):
+        print(f" C{i+1} ", end="  ")
+    print()
+
+    # isi board
+    for i in range(size):
+        print(f"R{i+1}", end="")
+        
+        for j in range(size):
+            print(f"|{play_box[i][j]:^3}", end="")
+            
+        print("|")
         
 def check(baris,kolom):
     display = 0
@@ -52,73 +61,88 @@ def check(baris,kolom):
         nx = baris + dx
         ny = kolom + dy
 
-        if 0 <= nx < 5 and 0 <= ny < 5:
-            if isi[nx][ny] == box[0]:
+        if 0 <= nx < size and 0 <= ny < size:
+            if inside[nx][ny] == box[0]:
                 display += 1
 
-    table[baris][kolom] = f" {numbers[display]} "
+    play_box[baris][kolom] = f" {numbers[display]} "
     
 def win():
     iswinning = True
     
-    for i in range(5):
-        for j in range(5):
-            if isi[i][j] != box[3] and table[i][j] == box[4]:
+    for i in range(size):
+        for j in range(size):
+            if inside[i][j] != box[3] and play_box[i][j] == box[4]:
                 iswinning = False
                 
     return iswinning
     
 def boom():
-    for i in range(5):
-        for j in range(5):
-            if isi[i][j] == box[0]:
-                table[i][j] = box[0]
+    for i in range(size):
+        for j in range(size):
+            if inside[i][j] == box[0]:
+                play_box[i][j] = box[0]
             # PSEUDOCODE
-            # KALAU isi(indeks saat ini) == bomb:
-            #       table(indeks saat ini) => bomb
+            # KALAU inside(indeks saat ini) == bomb:
+            #       play_box(indeks saat ini) => bomb
             
         
 #loop
 while True:
     try:
         board()
-        print("1.open a box\n2.flag a box")
+        print("1. Open a box\n2. Flag a box/Remove flag")
         choice = int(input("pick:"))
         if choice == 1:
-            print("pilih 0 untuk cancel")
-            baris = int(input("pilih baris ke:"))-1
-            kolom = int(input("pilih kolom ke:"))-1
-            
-            if baris == -1 or kolom == -1:
+            print("Type 0 to cancel")
+            baris = int(input("Pick row (R):"))-1
+
+            if baris == -1:
                 raise ValueError
             
-            if table[baris][kolom] != box[4]:
-                input("harus pilih kotak kosong!")
+            kolom = int(input("Pick column (C):"))-1
+            
+            if kolom == -1:
+                raise ValueError
+            
+            if play_box[baris][kolom] != box[4]:
+                input("Must choose an empty box!")
                 os.system("cls")
                 continue
             
-            elif isi[baris][kolom] == box[0]:
-                for i in range (len(table)):
-                    for j in range (len(table[i])):
-                        if isi[i][j] == box[0]:
-                            table[i][j] = box[0]
-                table[baris][kolom] = box[2]
+            elif inside[baris][kolom] == box[0]:
+                for i in range (len(play_box)):
+                    for j in range (len(play_box[i])):
+                        if inside[i][j] == box[0]:
+                            play_box[i][j] = box[0]
+                play_box[baris][kolom] = box[2]
                 os.system("cls")
                 board()
                 print("Game Over! It's a mine. 💣 ")
                 break
                 
-            elif isi[baris][kolom] == box[1]:
+            elif inside[baris][kolom] == box[1]:
                 check(baris,kolom)
-                input("bukan bomb!")
+                input("Not a mine!")
                 os.system("cls")
             
          #flag
         elif choice == 2:
-            baris = int(input("pilih baris ke:"))-1
-            kolom = int(input("pilih kolom ke:"))-1
-            if table[baris][kolom] == box[4]:
-                table[baris][kolom] = box[3]
+            print("Type 0 to cancel")
+            baris = int(input("Pick row (R):"))-1
+
+            if baris == -1:
+                raise ValueError
+            kolom = int(input("Pick column (C):"))-1
+
+            if kolom == -1:
+                raise ValueError
+            if play_box[baris][kolom] == box[4]:
+                play_box[baris][kolom] = box[3]
+            elif play_box[baris][kolom] == box[3]:
+                play_box[baris][kolom] = box[4]
+            else:
+                input("Already opened")
             os.system("cls")
         
         if win():
